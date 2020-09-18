53ca2a38d2ed4832c199d82315517796036450b1
Kimbsy <lordkimber@gmail.com>
2020-09-18 17:24:54 +0100

    Add basic platforms
    Adding platforms that the player can land on, as well as gravity and
    friction which apply to the player depending on if it has landed or not.

--- src/dynamically_typed/scenes/level_1.clj
+++ src/dynamically_typed/scenes/level_1.clj
@@ -1,14 +1,18 @@
 (ns dynamically-typed.scenes.level-1
   (:require [dynamically-typed.command :as command]
-            [dynamically-typed.utils :as u]
             [dynamically-typed.sound :as sound]
+            [dynamically-typed.utils :as u]
+            [quil.core :as q]
+            [quip.collision :as qpcollision]
             [quip.scene :as qpscene]
             [quip.sprite :as qpsprite]
             [quip.utils :as qpu]))
 
 (defn update-level
   [state]
-  state)
+  (-> state
+      qpcollision/update-collisions
+      qpscene/update-scene-sprites))
 
 (defn draw-level
   [state]
@@ -18,11 +22,31 @@
 
 (defn init-player
   []
-  (qpsprite/animated-sprite :player
-                            [200 200]
-                            20
-                            20
-                            "img/player.png"))
+  (-> (qpsprite/animated-sprite :player
+                                [200 200]
+                                32
+                                32
+                                "img/player.png"
+                                :update-fn (comp qpsprite/update-animated-sprite
+                                                 u/apply-gravity
+                                                 u/apply-friction))
+      (merge {:landed    false
+              :direction [1 1]})))
+
+(defn init-platforms
+  []
+  [(qpsprite/static-sprite :platforms
+                           [600 650]
+                           1200
+                           50
+                           "img/player.png" ; hack, we're not using an
+                                            ; image yet
+                           :draw-fn (fn [{[x y] :pos w :w h :h}]
+                                      (qpu/fill qpu/grey)
+                                      (q/rect (- x (/ w 2))
+                                              (- y (/ h 2))
+                                              w
+                                              h)))])
 
 (defn key-pressed-fns
   []
@@ -30,25 +54,60 @@
    command/clear])
 
 (defn jump
-  [state]
+  [{:keys [current-scene] :as state}]
   (prn "**JUMPING**")
   (sound/jump)
-  state)
+  (let [sprites     (get-in state [:scenes current-scene :sprites])
+        non-players (remove #(#{:player} (:sprite-group %)) sprites)
+        player      (first (filter #(#{:player} (:sprite-group %)) sprites))]
+    (prn (:vel player))
+    (assoc-in state
+              [:scenes current-scene :sprites]
+              (conj non-players
+                    (-> player
+                        (update :vel (fn [[vx vy]] [vx (- vy 5)]))
+                        (update :pos (fn [[x y]] [x (- y 10)]))
+                        (assoc :landed false))))))
 
 (defn dash
-  [state]
+  [{:keys [current-scene] :as state}]
   (prn "**DASHING**")
   (sound/dash)
-  state)
+  (let [sprites     (get-in state [:scenes current-scene :sprites])
+        non-players (remove #(#{:player} (:sprite-group %)) sprites)
+        player      (first (filter #(#{:player} (:sprite-group %)) sprites))
+        direction   (:direction player)]
+    (assoc-in state
+              [:scenes current-scene :sprites]
+              (conj non-players
+                    (-> player
+                        (update :vel (fn [vel]
+                                       (map + vel
+                                            (map * direction
+                                                 [10 0])))))))))
+
+(defn player-landed
+  [p]
+  (-> p
+      (update :vel (fn [[vx vy]]
+                     [vx 0]))
+      (assoc :landed true)))
 
 (defn init
   []
   {:update-fn       update-level
    :draw-fn         draw-level
-   :sprites         [(init-player)]
+   :sprites         (concat [(init-player)]
+                            (init-platforms))
    :commands        {:jump (command/->command ["jump"  "hop" "leap"] jump)
                      :dash  (command/->command ["dash" "run" "move" "go"] dash)
                      :skip (command/->command ["skip"] identity)
                      :corroborate (command/->command ["corroborate"] identity)
                      :interfere (command/->command ["interfere"] identity)}
-   :key-pressed-fns (key-pressed-fns)})
+   :key-pressed-fns (key-pressed-fns)
+   :colliders [(qpcollision/collider
+                :player
+                :platforms
+                player-landed
+                identity
+                :collision-detection-fn qpcollision/w-h-rects-collide?)]})
--- src/dynamically_typed/utils.clj
+++ src/dynamically_typed/utils.clj
@@ -5,3 +5,23 @@
 (def alpha? #{\A \a \B \b \C \c \D \d \E \e \F \f \G \g \H \h \I \i \J \j \K \k
               \L \l \M \m \N \n \O \o \P \p \Q \q \R \r \S \s \T \t \U \u \V \v
               \W \w \X \x \Y \y \Z \z})
+
+(defn zero-ish
+  [i]
+  (if (< i 0.01)
+    0
+    i))
+
+(defn apply-gravity
+  [{:keys [landed] :as s}]
+  (if-not landed
+    (update s :vel (fn [[vx vy]]
+                     [vx (+ vy 0.1)]))
+    s))
+
+(defn apply-friction
+  [{:keys [landed] :as s}]
+  (if landed
+    (update s :vel (fn [[vx vy]]
+                     [(zero-ish (* vx 0.95)) vy]))
+    s))
